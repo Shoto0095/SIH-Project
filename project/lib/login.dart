367f7aad0062ad5b93679ccdcbcc728dde7fd0c1
shirshakk <kshirshak76@gmail.com>
2024-09-02 21:59:36 +0530

    First page Update

--- project/lib/login.dart
+++ project/lib/login.dart
@@ -8,11 +8,11 @@ class Welogin extends StatefulWidget {
 }
 
 class _WeloginState extends State<Welogin> {
-  final List<String> DepartmentOptions = [
-    'Department 1',
-    'Department 2',
-    'Department 3',
-    
+  final List<String> passwordOptions = [
+    'Password 1',
+    'Password 2',
+    'Password 3',
+    // Add more password options here
   ];
 
   String? selectedPassword;
@@ -33,38 +33,132 @@ class _WeloginState extends State<Welogin> {
           child: Column(
             children: [
               Padding(
-                padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.28,
-                  left: 40,
-                  right: 40, // Add right padding to center horizontally
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment
-                      .center, // Center the elements horizontally
-                  children: [
-                    Column(
-                      children: [
-                        Text('Geo',
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold)),
-                        Text('Attend',
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                    SizedBox(width: 10),
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assest/logo.png'),
-                      radius: 30.0,
+                padding: const EdgeInsets.only(top: 150, left: 40, right: 0),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: CircleAvatar(
+                      backgroundImage: const AssetImage('assest/logo.png'),
+                      radius: 50.0,
                       backgroundColor: Colors.transparent,
                     ),
-                  ],
+                  ),
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Username',
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.3),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter User Name',
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.3),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    filled: true,
+                    fillColor: Colors.white.withOpacity(0.3),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  items: passwordOptions.map((String option) {
+                    return DropdownMenuItem<String>(
+                      value: option,
+                      child: Text(option),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    // Handle dropdown value change
+                    setState(() {
+                      selectedPassword = value!;
+                    });
+                  },
+                  value: selectedPassword,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle Signin button click
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  child: const Text(
+                    'Signin',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+                child: Text(
+                  'OR',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: OutlinedButton(
+                  onPressed: () {
+                    // Handle Continue as Guest button click
+                  },
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  child: const Text(
+                    'Continue as Guest',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
