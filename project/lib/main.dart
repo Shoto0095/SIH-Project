92d0d2519b12301326cfa1695ed8d84cc6b98e09
shirshakk <kshirshak76@gmail.com>
2024-09-18 12:27:42 +0530

    Manual Login

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/Geolocation.dart';
+import 'package:project/ManualLogin.dart';
 import 'package:project/Screen2.dart';
 import 'package:project/UserHomePage.dart';
 import 'package:project/login.dart';
@@ -9,14 +10,14 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'login',
-    //home: Summary(),
-    routes: {
-      'login': (context) => const Welogin(),
-      'Screen2': (context) => const Screen2(),
-      'UserHomePage': (context) => const UserHomePage(),
-      'Geolocation': (context) => const Geolocation(),
-      'summary': (context) => const Summary(),
-      'DeviceLocation': (context) => const Geolocation(),
-    },
+    home: ManualAttendance(),
+    // routes: {
+    //   'login': (context) => const Welogin(),
+    //   'Screen2': (context) => const Screen2(),
+    //   'UserHomePage': (context) => const UserHomePage(),
+    //   'Geolocation': (context) => const Geolocation(),
+    //   'summary': (context) => const Summary(),
+    //   'DeviceLocation': (context) => const Geolocation(),
+    // },
   ));
 }
